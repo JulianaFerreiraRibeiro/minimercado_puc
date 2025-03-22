a2c68eda75ad4980f424869d44cab54144ebe585
Juliana Ferreira Ribeiro <julianaferreiraribeiro04@gmail.com>
2025-03-21 21:07:28 -0300

    corrigindo caminho das imagens

--- js/produtos.js
+++ js/produtos.js
@@ -9,9 +9,10 @@ const gerarProdutoHTML = (produto, lista) => {
     li.className = "bg-white rounded p-3 d-flex flex-column align-items-center";
 
     let imagem;
-    const basePath = window.location.pathname.includes("index.html") ? '' : '/minimercado_puc';
+    const basePath = window.location.hostname === "julianaferreiraribeiro.github.io" ? '/minimercado_puc' : '';
     
     imagem = `${basePath}/images/${produto.imagem}`;
+    
     
     
 
